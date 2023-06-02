fccb3b4037bb525b912c2b5e930b911d8891fe8e
geekxByte <115074475+geekxByte@users.noreply.github.com>
2023-06-02 23:18:34 +0530

    Create House Robber.cpp

---
+++ House Robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prevofprev=0;
+        for(int i=1;i<n;i++){
+            int include=nums[i];
+            if(i>1)
+                include+=prevofprev;
+            int notInclude=0+prev;
+            int curr=max(include,notInclude);
+            prevofprev=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+};
