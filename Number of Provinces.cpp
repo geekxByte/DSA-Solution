a0b40527b86d156ba38a7e41df11994dcb9ea493
geekxByte <115074475+geekxByte@users.noreply.github.com>
2023-06-04 19:41:57 +0530

    Create Number of Provinces.cpp

---
+++ Number of Provinces.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+   void dfs(int node, vector<int> adj[], vector<int>& vis){
+	  vis[node]=1;
+	  for(auto it: adj[node]){
+		  if(!vis[it]){
+			  dfs(it, adj, vis);
+		    }
+	    }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> adj[n];
+        vector<int> vis(n, 0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                cnt++;
+                dfs(i,adj,vis);
+            }
+        }
+        return cnt;
+    }
+};
