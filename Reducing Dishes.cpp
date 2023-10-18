23bc58a1da5ae15e6aa9e79e15741b137e27c9c6
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-10-18 21:29:41 +0530

    Create Reducing Dishes.cpp

---
+++ Reducing Dishes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<int>& arr,int idx,int n,int &maxi,int mul,vector<vector<int>>&dp){
+        if(idx>=n)
+            return 0;
+        if(dp[idx][mul]!=-1)
+            return dp[idx][mul];
+
+        int pick=(arr[idx]*mul)+solve(arr,idx+1,n,maxi,mul+1,dp);
+        int notPick=solve(arr,idx+1,n,maxi,mul,dp);
+        
+        maxi=max(pick,notPick);
+        return dp[idx][mul]=maxi;
+    }
+    int maxSatisfaction(vector<int>& arr) {
+        int maxi=0,mul=1;
+        sort(begin(arr),end(arr));
+        int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(arr,0,arr.size(),maxi,mul,dp);
+        
+    }
+};
