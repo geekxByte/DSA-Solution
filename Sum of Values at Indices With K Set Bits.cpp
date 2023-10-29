9474d969e8b0c1e610d66d9fc6f0c78117e7c83f
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-10-29 20:55:53 +0530

    Create Sum of Values at Indices With K Set Bits.cpp

---
+++ Sum of Values at Indices With K Set Bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(__builtin_popcount(i)==k)
+                sum+=nums[i];
+        }
+        return sum;
+    }
+};
