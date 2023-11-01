4f0711ed45336c8ec147a631294e4b1842336b1a
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-11-01 18:51:08 +0530

    Create Find The Original Array of Prefix Xor.cpp

---
+++ Find The Original Array of Prefix Xor.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> pre(pref.size());
+        pre[0]=pref[0];
+        for(int i=1;i<pref.size();i++){
+            pre[i]=pref[i]^pref[i-1];
+        }
+    return pre;
+    }
+};
