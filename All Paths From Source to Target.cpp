1fe1032ad6056bfad49931b9061958c2e9a6de78
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-11-02 22:04:04 +0530

    Create All Paths From Source to Target.cpp

---
+++ All Paths From Source to Target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void dfs(int node,vector<vector<int>>& graph,vector<vector<int>>& ans,vector<int> temp,int d){
+        //vis[node]=1;
+        temp.push_back(node);
+        if(node==d){
+            ans.push_back(temp);
+        }
+        for(auto it: graph[node]){
+            dfs(it,graph,ans,temp,d);
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int s=0,d=graph.size()-1;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        dfs(s,graph,ans,temp,d);
+        return ans;
+    }
+};
