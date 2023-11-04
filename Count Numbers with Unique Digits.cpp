98746ea9bf7363ea968011d02f680eb2f1958e6f
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-11-04 21:04:43 +0530

    Create Count Numbers with Unique Digits.cpp

---
+++ Count Numbers with Unique Digits.cpp
@@ -0,0 +1,34 @@
+class Solution { 
+private:
+    int backtrack(vector<bool>& chosen, int k, int n) {
+        
+        if (k < 1)
+            return 1;
+
+        int numUnique = 0;
+
+        for (int j = 0; j <= 9; ++j)
+        {
+            if ((chosen[j] == false) && (j != 0 || k != n))
+            {
+                chosen[j] = true;
+                numUnique += backtrack(chosen, k - 1, n);
+                chosen[j] = false;
+            }
+        }
+
+        return numUnique;
+    }
+
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        
+        int count = 0;
+        vector<bool> chosen(10, false);
+
+        for (int i = 0; i <= n; ++i)
+            count += backtrack(chosen, i, i);
+
+        return count;
+    }
+};
