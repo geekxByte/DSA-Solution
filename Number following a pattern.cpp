dc5ad951d546ab991e76406252f88523a0cab542
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-11-10 20:55:48 +0530

    Create Number following a pattern.cpp

---
+++ Number following a pattern.cpp
@@ -0,0 +1,20 @@
+class Solution{   
+public:
+    string printMinNumberForPattern(string S){
+        // code here 
+        string ans="";
+        vector<char>v;
+        for(int i=0;i<=S.size();i++) v.push_back(i+1+'0');
+        for(int i=1;i<v.size();++i){
+            if(S[i-1]=='D'){
+                int j=i;
+                while((j-1)<S.size() && S[j-1]=='D') j++;
+                int start=i-1,end=j-1;
+                while(start<end)swap(v[start++],v[end--]);
+                i=j-1;
+            }
+        }
+        for(int i=0;i<v.size();++i) ans.push_back(v[i]);
+        return ans;
+    }
+};
