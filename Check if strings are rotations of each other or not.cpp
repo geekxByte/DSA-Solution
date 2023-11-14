c843c5e810072163bcf15a7cbfeb771acc3ebe11
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-11-14 22:40:32 +0530

    Create Check if strings are rotations of each other or not.cpp

---
+++ Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool solve(string s1,string s2,int k){
+        for(int i=0;i<s1.size();i++){
+            if(s2[i]!=s1[(i+k)%s1.size()])
+                return false;
+        }
+        return true;
+    }
+    bool areRotations(string s1,string s2)
+    {
+        int k=0;
+        for(auto it: s1){
+            if(it==s2[0])
+                if(solve(s1,s2,k))
+                    return true;
+            k++;
+        }
+        return false;
+    }
+};
