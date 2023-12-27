3a80bf461c53d5bbd08d784751b84019656bb727
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2023-12-27 23:03:43 +0530

    Update Last Moment Before All Ants Fall Out of a Plank.cpp

--- Last Moment Before All Ants Fall Out of a Plank.cpp
+++ Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public: 
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int maxLeft = left.empty() ? 0 : *max_element(left.begin(), left.end());
 
