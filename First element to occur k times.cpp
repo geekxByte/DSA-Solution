63b48c3b2d9437f9af73193f3c38701065b21c6f
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2024-03-02 23:48:12 +0530

    Create First element to occur k times.cpp

---
+++ First element to occur k times.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[a[i]]++;
+            if(mpp[a[i]]==k)
+                return a[i];
+        }
+        return -1;
+    }
+};
